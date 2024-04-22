caab41c4c17f1ce117b149289630ab27a39a6384
Fruitkid <fruinch@gmail.com>
2024-04-22 22:54:01 +0800

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // ...
+  site: 'https://Fruitkid.github.io',
+  base: '/ECSS_website',
   integrations: [tailwind()]
 });
