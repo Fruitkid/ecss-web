ccdd6cd53329e7a9a7a59707db086ac06a00959a
wylited <44154103+wylited@users.noreply.github.com>
2024-04-22 22:06:59 +0800

    initial Astro website + react + shadcn/ui + db + tailwindcss

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config';
+import db from "@astrojs/db";
+import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [db(), sitemap(),
+  tailwind({
+    applyBaseStyles: false,
+  }), react()]
+});
