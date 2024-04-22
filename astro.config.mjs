c31c15926c6ed631568b57b5aaaf50bef07ea6f6
Fruitkid <fruinch@gmail.com>
2024-04-22 23:01:15 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fruitkid.github.io',
+  base: '/ECSS_website',
   integrations: [db(), sitemap(),
   tailwind({
     applyBaseStyles: false,
