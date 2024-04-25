66930ab81a390e957d372ddf8a05b33eced33279
wylited <44154103+wylited@users.noreply.github.com>
2024-04-25 22:09:17 +0800

    feat: opportunity page skeleton

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,146 +1,150 @@
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
+  NavigationMenu,
+  NavigationMenuLink,
+  NavigationMenuList,
 } from "@/components/ui/navigation-menu";
 import { Button } from "@/components/ui/button";
 import { SheetTrigger, SheetContent, Sheet } from "@/components/ui/sheet";
-import { ModeToggle } from '@/components/ModeToggle';
+import { ModeToggle } from "@/components/ModeToggle";
 
 export function TopNav() {
-    return (
-        <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
-            <a className="mr-6 hidden lg:flex" href="#">
-                <MountainIcon className="h-6 w-6" />
-                <span className="sr-only">ECSS</span>
+  return (
+    <header className="flex h-20 w-full shrink-0 items-center px-4 md:px-6">
+      <a className="mr-6 hidden lg:flex" href="/">
+        <MountainIcon className="h-6 w-6" />
+        <span className="sr-only">ECSS</span>
+      </a>
+      <div className="flex-1 flex justify-center">
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList className="flex justify-between w-full mr-4">
+            <NavigationMenuLink asChild>
+              <a
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                href="/Community"
+              >
+                Community
+              </a>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <a
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                href="/Opportunities"
+              >
+                Opportunities
+              </a>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <a
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                href="/Feed"
+              >
+                Feed
+              </a>
+            </NavigationMenuLink>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <div className="flex justify-end">
+        <NavigationMenu className="hidden lg:flex">
+          <NavigationMenuList className="flex justify-end w-full mr-4">
+            <NavigationMenuLink asChild>
+              <a
+                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
+                href="/Join"
+              >
+                Join
+              </a>
+            </NavigationMenuLink>
+            <NavigationMenuLink asChild>
+              <ModeToggle />
+            </NavigationMenuLink>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="lg:hidden" size="icon" variant="outline">
+            <MenuIcon className="h-6 w-6" />
+            <span className="sr-only">Toggle navigation menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left">
+          <div className="flex flex-row justify-between">
+            <a href="/">
+              <MountainIcon className="h-10 w-10" />
             </a>
-            <div className="flex-1 flex justify-center">
-                <NavigationMenu className="hidden lg:flex">
-                    <NavigationMenuList className="flex justify-between w-full mr-4">
-                        <NavigationMenuLink asChild>
-                            <a
-                                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                                href="/Community"
-                            >
-                                Community
-                            </a>
-                        </NavigationMenuLink>
-                        <NavigationMenuLink asChild>
-                            <a
-                                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                                href="/"
-                            >
-                                Opportunities
-                            </a>
-                        </NavigationMenuLink>
-                        <NavigationMenuLink asChild>
-                            <a
-                                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                                href="/Projects"
-                            >
-                                Projects
-                            </a>
-                        </NavigationMenuLink>
-                    </NavigationMenuList>
-                </NavigationMenu>
+            <div className="pr-6">
+              <ModeToggle />
             </div>
-            <div className="flex justify-end">
-                <NavigationMenu className="hidden lg:flex">
-                    <NavigationMenuList className="flex justify-end w-full mr-4">
-                        <NavigationMenuLink asChild>
-                            <a
-                                className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
-                                href="/Join"
-                            >
-                                Join
-                            </a>
-                        </NavigationMenuLink>
-                        <NavigationMenuLink asChild>
-                            <ModeToggle />
-                        </NavigationMenuLink>
-                    </NavigationMenuList>
-                </NavigationMenu>
-            </div>
-            <Sheet>
-                <SheetTrigger asChild>
-                    <Button className="lg:hidden" size="icon" variant="outline">
-                        <MenuIcon className="h-6 w-6" />
-                        <span className="sr-only">Toggle navigation menu</span>
-                    </Button>
-                </SheetTrigger>
-                <SheetContent side="left">
-                    <div className="flex flex-row justify-between">
-                    <a href="#">
-                        <MountainIcon className="h-10 w-10" />
-                    </a>
-                    <div className="pr-6">
-                    <ModeToggle/>
-                    </div>
-                    </div>
-                        <div className="grid gap-2 py-6">
-                            <a className="flex w-full items-center py-2 text-lg font-semibold" href="/Community/">
-                                Community
-                    </a>
-                            <a className="flex w-full items-center py-2 text-lg font-semibold" href="/Opportunities/">
-                                Opportunities
-                                
-                    </a>
-                            <a className="flex w-full items-center py-2 text-lg font-semibold" href="/Projects/">
-                                Projects
-                                
-                    </a>
-                            <a className="flex w-full items-center py-2 text-lg font-semibold" href="/Join">
-                                Join
-                                
-                    </a>
-                    
-                      </div>
-                </SheetContent>
-            </Sheet>
-        </header>
-    );
+          </div>
+          <div className="grid gap-2 py-6">
+            <a
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/Community/"
+            >
+              Community
+            </a>
+            <a
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/Opportunities/"
+            >
+              Opportunities
+            </a>
+            <a
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/Projects/"
+            >
+              Projects
+            </a>
+            <a
+              className="flex w-full items-center py-2 text-lg font-semibold"
+              href="/Join"
+            >
+              Join
+            </a>
+          </div>
+        </SheetContent>
+      </Sheet>
+    </header>
+  );
 }
 
 function MenuIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <line x1="4" x2="20" y1="12" y2="12" />
-            <line x1="4" x2="20" y1="6" y2="6" />
-            <line x1="4" x2="20" y1="18" y2="18" />
-        </svg>
-    )
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <line x1="4" x2="20" y1="12" y2="12" />
+      <line x1="4" x2="20" y1="6" y2="6" />
+      <line x1="4" x2="20" y1="18" y2="18" />
+    </svg>
+  );
 }
 
-
 function MountainIcon(props) {
-    return (
-        <svg
-            {...props}
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-        >
-            <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
-        </svg>
-    )
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="m8 3 4 8 5-5 5 15H2L8 3z" />
+    </svg>
+  );
 }
--- src/components/ui/grid.tsx
+++ src/components/ui/grid.tsx
@@ -1,13 +1,15 @@
-import * as React from "react"
+import * as React from "react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 export default function BentoGrid() {
   return (
-<div className="container mx-auto my-16 px-4 sm:px-6 lg:px-8">
+    <div className="container mx-auto my-16 px-4 sm:px-6 lg:px-8">
       <div className="grid grid-cols-3 space-x-8">
-        <a className="col-span-1 row-span-2 bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group" href="#">
-          
+        <a
+          className="col-span-1 row-span-2 bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group"
+          href="#"
+        >
           <div className="flex h-full group-hover:scale-105 transition-transform duration-300 ease-in-out">
             <img
               alt="Could be an image"
@@ -23,59 +25,68 @@ export default function BentoGrid() {
           </div>
         </a>
         <div className="col-span-2 grid grid-cols-2 gap-10">
-        <a 
-          className="bg-gray-100 dark:bg-gray-800 rounded-lg p-10 flex flex-col items-center justify-between group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
-          href="/src/pages/Community"
-        >
-          <div className="text-center space-y-10">
-            <h3 className="text-3xl font-semibold">Community</h3>
-            <p className="text-gray-500 dark:text-gray-400 mt-2">Learn more about our society and community.</p>
-          </div>
+          <a
+            className="bg-gray-100 dark:bg-gray-800 rounded-lg p-10 flex flex-col items-center justify-between group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
+            href="/src/pages/Community"
+          >
+            <div className="text-center space-y-10">
+              <h3 className="text-3xl font-semibold">Community</h3>
+              <p className="text-gray-500 dark:text-gray-400 mt-2">
+                Learn more about our society and community.
+              </p>
+            </div>
             <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
               Learn More
             </Button>
-        </a>
-        <a
-          className="bg-gray-100 dark:bg-gray-800 rounded-lg p-10 flex flex-col items-center justify-between group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
-          href="/src/pages/Opportunities">
-          <div className="text-center space-y-10">
-            <h3 className="text-3xl font-semibold">Opportunities</h3>
-            <p className="text-gray-500 dark:text-gray-400 mt-2">Find local opportunties in the computer science space.</p>
-          </div>
+          </a>
+          <a
+            className="bg-gray-100 dark:bg-gray-800 rounded-lg p-10 flex flex-col items-center justify-between group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
+            href="/src/pages/Opportunities"
+          >
+            <div className="text-center space-y-10">
+              <h3 className="text-3xl font-semibold">Opportunities</h3>
+              <p className="text-gray-500 dark:text-gray-400 mt-2">
+                Find local opportunties in the computer science space.
+              </p>
+            </div>
             <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
               Check them out
             </Button>
-
-        </a>
-        <a className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12" href="/src/pages/Projects">
-          <div className="flex h-full items-center justify-center p-10">
-            <div className="text-center space-y-10">
-              <h4 className="text-3xl font-semibold">Projects</h4>
-              <p className="text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50 transition-colors duration-300">
-              Learn about student projects and maybe join them.
-              </p>
-              <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
-                Projects here
-              </Button>
+          </a>
+          <a
+            className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
+            href="/src/pages/Projects"
+          >
+            <div className="flex h-full items-center justify-center p-10">
+              <div className="text-center space-y-10">
+                <h4 className="text-3xl font-semibold">Projects</h4>
+                <p className="text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50 transition-colors duration-300">
+                  Learn about student projects and maybe join them.
+                </p>
+                <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
+                  Projects here
+                </Button>
+              </div>
             </div>
-          </div>
-        </a>
-        <a className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12" href="/src/pages/About">
-          <div className="flex h-full items-center justify-center p-6  space-y-12">
-            <div className="text-center space-y-10">
-              <h4 className="text-3xl font-semibold">About Us</h4>
-              <p className="text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50 transition-colors duration-300">
-                Administrators and founders..
-              </p>
-              <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
-                Learn more
-              </Button>
+          </a>
+          <a
+            className="bg-gray-100 dark:bg-gray-800 rounded-lg overflow-hidden group hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors duration-300 space-y-12"
+            href="/src/pages/About"
+          >
+            <div className="flex h-full items-center justify-center p-6  space-y-12">
+              <div className="text-center space-y-10">
+                <h4 className="text-3xl font-semibold">About Us</h4>
+                <p className="text-gray-500 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-gray-50 transition-colors duration-300">
+                  Administrators and founders..
+                </p>
+                <Button className="group-hover:bg-gray-900 group-hover:text-gray-50 transition-colors duration-300">
+                  Learn more
+                </Button>
+              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
     </div>
-    </div>
-    
-    )
-    }
+  );
+}
